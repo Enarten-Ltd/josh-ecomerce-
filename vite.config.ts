11c491ce109573187b326bb9e51c95b27dc80531
EnartenLtd <enartenservices@gmail.com>
2025-12-03 10:11:49 +0100

    Update vite.config.ts
    corrected file structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],]
-  base: "/josh-ecommerce-/",
+  plugins: [react()],
+  base: "/josh-ecomerce-/", // make sure this matches your repo name
 })
